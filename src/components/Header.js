b421bdde3e292b8146e8c3eb9c45d07ac128c49a
MH Hamza <mhamza2551@gmail.com>
2020-10-17 14:38:47 +0500

    Create Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Header extends React.Component {
+    render() {
+        return (
+            <div className='bg-primary text-white'>
+                <div className='display-3 container text-center'>Todo App</div>
+                <br />
+            </div>  
+        )
+    }
+}
+
+export default Header
