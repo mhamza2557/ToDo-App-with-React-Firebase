3064ab9c729e515534b3095fac7a6b1ddab4c8e3
MH Hamza <mhamza2551@gmail.com>
2020-10-17 14:38:44 +0500

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Footer extends React.Component {
+    render() {
+        return (
+            <div style={{fontSize:'30px'}} className='text-white bg-primary text-center fixed-bottom'>
+                Footer
+            </div>
+        )
+    }
+}
+
+export default Footer
